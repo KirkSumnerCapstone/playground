9e0069c0a88d83eb7542d6b48fd83e1787bdf703
tempuser <ksumner@ksumner-mbp.local>
2018-06-07 07:04:01 -0500

    Initial project setup

---
+++ Playground/Playground macOS/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  Playground macOS
+//
+//  Created by Kirk Sumner on 6/7/18.
+//  Copyright © 2018 Kirk Sumner. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ Playground/Playground macOS/GameViewController.swift
@@ -0,0 +1,31 @@
+//
+//  GameViewController.swift
+//  Playground macOS
+//
+//  Created by Kirk Sumner on 6/7/18.
+//  Copyright © 2018 Kirk Sumner. All rights reserved.
+//
+
+import Cocoa
+import SpriteKit
+import GameplayKit
+
+class GameViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let scene = GameScene.newGameScene()
+        
+        // Present the scene
+        let skView = self.view as! SKView
+        skView.presentScene(scene)
+        
+        skView.ignoresSiblingOrder = true
+        
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+    }
+
+}
+
